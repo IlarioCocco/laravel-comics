6c9a7a7ccd884b50374b5bfd8888362c25bfc43d
IlarioCOCCO <ilario.cocco@gmail.com>
2021-11-05 19:20:24 +0100

    add img

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,14 @@
 <header>
+    <div class="container">
+        <div class="logo">
+            <img src="" alt="logo Dc Comics">
+            <ul>
+                <li><a href="#">prova</a></li>
+                <li><a href="#">prova</a></li>
+                <li><a href="#">prova</a></li>
+            </ul>
+        </div>
+
+    </div>
     
 </header>
