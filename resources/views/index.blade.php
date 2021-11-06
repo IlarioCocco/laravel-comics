e77c50632bc2dbc72ac3c9baae6303a397543f72
IlarioCOCCO <ilario.cocco@gmail.com>
2021-11-06 01:01:56 +0100

    add header

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,9 @@
 @extends('layout.base')
 
 @section('pageContent')
+
     <section>
-         <h4>prova</h4>
+       
     </section>
    
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,14 +1,26 @@
 <header>
     <div class="container">
-        <div class="logo">
-            <img src="" alt="logo Dc Comics">
-            <ul>
-                <li><a href="#">prova</a></li>
-                <li><a href="#">prova</a></li>
-                <li><a href="#">prova</a></li>
-            </ul>
+        <div class="lineBluHeader">
+            <h4>dc power<span>sm</span>visa</h4>
+            <h4 class="textUpDc">additional dc sites</h4>
         </div>
-
+        <div class="headerWhite">
+            <img class="logoDc" src="{{asset("images/dc-logo.png")}}" alt="logo Dc Comics">
+            <ul class="listato">
+                <li><a href="#">charaters</a></li>
+                <li><a href="#">comics</a></li>
+                <li><a href="#">movies</a></li>
+                <li><a href="#">tv</a></li>
+                <li><a href="#">games</a></li>
+                <li><a href="#">collectibles</a></li>
+                <li><a href="#">videos</a></li>
+                <li><a href="#">fans</a></li>
+                <li><a href="#">shop</a></li>
+            </ul>
+            <h5 class="search">Search</h5>
+        </div>
     </div>
-    
+    <div class="jumboUp">
+        <img src="" alt="">
+    </div>
 </header>
