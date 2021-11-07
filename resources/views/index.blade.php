781f63106e4b7869ea7b20cffdc281b25780f369
IlarioCOCCO <ilario.cocco@gmail.com>
2021-11-07 16:26:39 +0100

    add main prove with text lorem in @section/@endSec

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,15 @@
 @extends('layout.base')
 
-    <section>
-         <div class="container">
-        </div>
-    </section>
-
 @section('pageContent')
 
+ <main>
+     <div class="containerMain">
+        <div>
+            <h3>Prova</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ipsa debitis, impedit nemo reiciendis eaque dolorem hic modi. Natus possimus magni sunt nam omnis reprehenderit ad beatae odit doloribus! Quia!</p>
+        </div>
+     </div>
+ </main>
 
 @endsection
 
