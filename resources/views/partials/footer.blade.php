b15a62a3a59ee212e5019dad489c0d47ac338d8e
IlarioCOCCO <ilario.cocco@gmail.com>
2021-11-07 15:28:41 +0100

    add footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,91 @@
 <footer>
-    
+    <div class="container">
+
+        <div class="box">
+            <div class="footerBlu">
+            <div class="loghiSmart">
+                <img class="loghiSmart" src="{{asset("images/buy-comics-digital-comics.png")}}" alt="">
+                <h4 class="textSmart">prova footer</h4>
+            </div>
+            <div class="loghiSmart">
+                <img class="loghiSmart" src="{{asset("images/buy-comics-merchandise.png")}}" alt="">
+                <h4 class="textSmart">prova footer</h4>
+            </div>
+            <div class="loghiSmart">
+                <img class="loghiSmart" src="{{asset("images/buy-comics-subscriptions.png")}}" alt="">
+               <h4 class="textSmart">prova footer</h4>
+            </div>
+            <div class="loghiSmart">
+                <img class="loghiSmart" src="{{asset("images/buy-comics-shop-locator.png")}}" alt="">
+               <h4 class="textSmart">prova footer</h4>
+            </div>
+            <div class="loghiSmart">
+                <img class="loghiSmart" src="{{asset("images/buy-dc-power-visa.svg")}}" alt="">
+               <h4 class="textSmart">prova footer</h4>
+            </div>
+        </div>
+
+        </div>
+
+        
+
+    </div>
+
+
+    <div class="jumboDown">
+        <div class="liste">
+            <ul>
+                <li><span>titolo</span></li>
+                <li>saeaseat</li>
+                <li>saeaseat</li>
+                <li>saeaseat</li>
+                <li>saeaseat</li>
+                <li>saeaseat</li>
+                   
+            </ul>
+           
+            <ul>
+                <li><span>titolo</span></li>
+                <li>saeaseat</li>
+                <li>saeaseat</li>
+                <li>saeaseat</li>
+                <li>saeaseat</li>
+                <li>saeaseat</li>
+            </ul>
+        
+            <ul>
+                <li><span>titolo</span></li>
+                <li>saeaseat</li>
+                <li>saeaseat</li>
+                <li>saeaseat</li>
+                <li>saeaseat</li>
+                <li>saeaseat</li>
+            </ul>
+
+        </div>
+         
+        
+            <img class="logoDcDown"src="../images/dc-logo-bg.png" alt="">
+      
+    </div>
+
+    <div class="container">
+      
+        <div class="footerGrey">
+           
+            <button class="signButton">SIGN-UP NOW!</button>
+            
+
+            <div class="iconSocial">
+                <h4>follow us</h4>
+                <img class="loghiSmart iconSocial" src="{{asset("images/footer-facebook.png")}}" alt="">
+                <img class="loghiSmart iconSocial" src="{{asset("images/footer-twitter.png")}}" alt="">
+                <img class="loghiSmart iconSocial" src="{{asset("images/footer-youtube.png")}}" alt="">
+                <img class="loghiSmart iconSocial" src="{{asset("images/footer-pinterest.png")}}" alt="">
+                <img class="loghiSmart iconSocial" src="{{asset("images/footer-periscope.png")}}" alt="">
+            </div>  
+            
+        </div>
+       
+    </div>
 </footer>
