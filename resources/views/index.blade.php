2e72cfbf4550fcfd5ad4f1117c2ae69b3e635f47
IlarioCOCCO <ilario.cocco@gmail.com>
2021-11-07 16:54:03 +0100

    add title Home page laravel Comics with @section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,26 @@
 @extends('layout.base')
 
+
+@section('title')
+  Home Page laravel Comics
+@endsection()
+
+
+
+
+
+
+
+
+
+{{-- 
+section del main --}}
+
 @section('pageContent')
 
  <main>
      <div class="containerMain">
-        <div>
+        <div class="box">
             <h3>Prova</h3>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ipsa debitis, impedit nemo reiciendis eaque dolorem hic modi. Natus possimus magni sunt nam omnis reprehenderit ad beatae odit doloribus! Quia!</p>
         </div>
